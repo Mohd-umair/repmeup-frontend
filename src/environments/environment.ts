906ee93cb3bfe763dcc7878b3f475667027d3865
Mohammed Umair <umair9317@gmail.com>
2026-01-22 15:36:13 +0400

    umair

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,3 +3,11 @@ export const environment = {
   apiUrl: 'http://localhost:3000/api',
   socketUrl: 'http://localhost:3000'
 };
+
+
+// export const environment = {
+//   production: false,
+//   apiUrl: 'https://repmeup.in/api',
+//   socketUrl: 'https://repmeup.in/api'
+// };
+
